37649b3147757a93676a797cf6e2c2124feb9fda
Eric Lott <ericblott@gmail.com>
2025-06-27 17:04:57 -0500

    Revert "Add project files."
    This reverts commit 899c98ca3e1aeea0a506390c52ca2574a4b2680c.

--- LiquorApp.Server/Services/IBottleRecognitionService.cs
+++
@@ -1,9 +0,0 @@
-using LiquorApp.Server.Models;
-using Microsoft.AspNetCore.Http;
-
-namespace LiquorApp.Server.Services;
-
-public interface IBottleRecognitionService
-{
-    Task<BottleRecognitionResult> RecognizeBottleAsync(IFormFile image);
-}
